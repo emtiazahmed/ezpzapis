9cca8ed3d05412b7923f18db68d9c89d5a5ff733
Imtiaz Ahmed <emtiazahmed@gmail.com>
2023-10-23 23:37:05 -0700

    Configuring Kong and publishing website

--- buildImages.sh
+++ buildImages.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-modules=( tenants-service metastore-service )
+modules=( tenants-service metastore-service website )
 
 for module in "${modules[@]}"; do
     docker build -t "ezpzapis/${module}:latest" ${module} --no-cache
